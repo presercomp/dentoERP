992387cc90d1cc2392543066b2ba3a6d6610c8a1
PRESERCOMP <sebastian.solar@presercomp.cl>
2015-09-10 18:23:01 -0300

    Create db_structure.sql

---
+++ db_structure.sql
@@ -0,0 +1,9 @@
+-- DROP DATABASE "DentoERP";
+
+CREATE DATABASE "DentoERP"
+  WITH OWNER = postgres
+       ENCODING = 'UTF8'
+       TABLESPACE = pg_default
+       LC_COLLATE = 'Spanish_Spain.1252'
+       LC_CTYPE = 'Spanish_Spain.1252'
+       CONNECTION LIMIT = -1;
